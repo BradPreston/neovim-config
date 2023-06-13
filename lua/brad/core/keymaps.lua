94b758c9193ac8d5fd999dd88163182d1555ff1e
bradpreston <bap5393@gmail.com>
2023-06-13 15:21:01 -0400

    Initial commit

---
+++ lua/brad/core/keymaps.lua
@@ -0,0 +1,40 @@
+vim.g.mapleader = " " -- uses space as the leader for custom keymaps
+
+local keymap = vim.keymap -- for conciseness
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>")
+
+--general keymaps
+keymap.set("n", "<leader>nh", ":nohl<CR>") -- space + n + h clears search 
+keymap.set("n", "x", '"_x') -- x doesn't copy when clearing
+keymap.set("n", "<leader>+", "<C-a>") -- space + "+" increments a number
+keymap.set("n", "<leader>-", "<C-x>") -- space + "-" decrements a number
+
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
+
+keymap.set("n", "<leader>hh", "<C-w>h") -- navigate to left window
+keymap.set("n", "<leader>ll", "<C-w>l") -- navigate to right window
+keymap.set("n", "<leader>kk", "<C-w>k") -- navigate to upper window
+keymap.set("n", "<leader>jj", "<C-w>j") -- navigate to lower window
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file tree
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files in current project
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find text in current project
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find current string the cursor is on throughout the project
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- show current buffers
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- show help tags
---
+++ lua/brad/plugins/lsp/lspconfig.lua
@@ -0,0 +1,183 @@
+local lspconfig_status, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status then
+    return
+end
+
+local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_status then
+    return
+end
+
+local typescript_setup, typescript = pcall(require, "typescript")
+if not typescript_setup then
+    return
+end
+
+local keymap = vim.keymap
+
+-- enable keybinds for available lsp server
+local on_attach = function(client, bufnr)
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+
+    -- set keybinds
+    keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)
+    keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+    keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+    keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+    keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+    keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+    keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+    keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+    keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+    keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+
+    if client.name == "tsserver" then
+        keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>")
+    end
+end
+
+-- used to enable autocompletion
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig["html"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+typescript.setup({
+    server = {
+        capabilities = capabilities,
+        on_attach = on_attach
+    }
+})
+
+
+lspconfig["sqlls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["arduino_language_server"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["bashls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["cssls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["cssmodules_ls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["dockerls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["golangci_lint_ls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["gopls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["graphql"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["cssmodules_ls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["unocss"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["jsonls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["quick_lint_js"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["tsserver"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["vtsls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["marksman"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["prosemd_lsp"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["remark_ls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["zk"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["rust_analyzer"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["taplo"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["yamlls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach
+})
+
+lspconfig["lua_ls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.stdpath("config") .. "/lua"] = true,
+                },
+            },
+        },
+    },
+})
